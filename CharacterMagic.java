078f38e0791a29a003992522c7d0d67b8333a5e8
suyashtripathi333 <suyash.333.tripathi@gmail.com>
2026-07-05 15:33:32 +0530

    give small case character and convert it into upper case

---
+++ CharacterMagic.java
@@ -0,0 +1,21 @@
+package luv_babbar;
+import java.util.*;
+
+public class CharacterMagic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner sc = new Scanner(System.in);
+		 
+		 char M = 'a';
+		 
+		 int num = M;
+		 
+		 int change = num-32;
+		 
+		 char N = (char) (change);
+		 
+		 System.out.println(N);
+	}
+
+}
