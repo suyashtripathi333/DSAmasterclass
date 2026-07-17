10b8865c29b6480a20ccf668da32407a3e1a6485
suyashtripathi333 <suyash.333.tripathi@gmail.com>
2026-07-17 15:17:51 +0530

    Here is the rhombus pattern code

---
+++ patterns/rhombus.java
@@ -0,0 +1,32 @@
+package patterns;
+import java.util.*;
+public class rhombus {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Number of rows here");
+		int n =sc.nextInt();
+		
+		
+//		
+		for(int i =1; i<=n;i++) {
+			//this for loop is  for space n-row
+			for(int j =1; j<=(n-i);j++) {
+				System.out.print(" ");
+			}
+			// for printing stars
+			for(int j =0; j<=n; j++) {
+				System.out.print(" * ");
+				
+			}
+			System.out.println();
+		}
+		
+		
+
+	}
+
+}
