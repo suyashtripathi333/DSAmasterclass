6bc337676d67236c8c443fdb4bbaf8519579e3b1
suyashtripathi333 <suyash.333.tripathi@gmail.com>
2026-07-17 14:35:12 +0530

    This is right angle star pattern

---
+++ patterns/pattern2.java
@@ -0,0 +1,29 @@
+package patterns;
+import java.util.*;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		this is right angle pattern here
+		
+//		now take a row from user
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("How many rows right angle triangle u need");
+		int m = sc.nextInt();
+		
+		// now starting loop 
+		
+		for(int i=0; i<m;i++) {
+			for(int j =0; j<(i+1);j++) {
+				System.out.print(" * ");
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
