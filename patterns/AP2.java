b9022bfb7ef6a15220379da20580600a18975d8e
suyashtripathi333 <suyash.333.tripathi@gmail.com>
2026-07-19 16:31:03 +0530

    this alpha numeric pattern 2

---
+++ patterns/AP2.java
@@ -0,0 +1,26 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class AP2 {
+
+    static void main(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Here give the input ");
+        int row = sc.nextInt();
+
+
+        for(int i = 1 ; i <= row ; i++){
+            int k =69 ;
+
+            for(int j = 1 ; j <= i ; j++){
+                char m = (char)k;
+                System.out.print(m + " ");
+                k= k-1;
+            }
+            System.out.println();
+        }
+    }
+}
