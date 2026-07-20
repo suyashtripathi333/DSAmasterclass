e09dab8b2b63c0a2e8837b76d057c0064737fad1
suyashtripathi333 <suyash.333.tripathi@gmail.com>
2026-07-20 16:21:52 +0530

    Initial method example

---
+++ methods/MethodExample.java
@@ -0,0 +1,16 @@
+package methods;
+
+import java.util.*;
+
+public class MethodExample {
+    static void print2Table(){
+        for(int i =1 ; i<=10; i++){
+            System.out.println("2 "+ "  * "+i+" = "+ 2*i);
+        }
+    }
+    static void main(){
+
+        print2Table();
+
+    }
+}
