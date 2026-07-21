a4c1c117ac121b2d5401ac514efff8c845678c51
suyashtripathi333 <suyash.333.tripathi@gmail.com>
2026-07-21 13:30:56 +0530

    It is the greeting message using functions to print

---
+++ methods/WelcomeMessage.java
@@ -0,0 +1,16 @@
+package methods;
+ import java.util.*;
+
+public class WelcomeMessage {
+
+    static void welcome(){
+        System.out.println("Welcome to methods part of java");
+    }
+
+    static void main(){
+
+        welcome();
+
+
+    }
+}
